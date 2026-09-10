8ab94dd52ccf5ce6c23c154650d7ca1f62e584df
asm-saim <saim777m@gmail.com>
2026-09-10 08:25:13 +0600

    available , selected button handler implemeneted and state managed

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type { IPlayers } from "./type";
 import AvailablePlayer from "./AvailablePlayer";
 interface PlayersProps {
@@ -7,21 +7,35 @@ interface PlayersProps {
 
 const Players = ({ playersInfo }: PlayersProps) => {
   const playersInformation = use(playersInfo);
+
+  //
+  const [buttonType, setButtonType] = useState("available");
+
+  const handleButtonType = (type: "available" | "selected") => {
+    setButtonType(type);
+  };
+
   return (
     <div className="max-w-6xl mx-auto">
       <h2>{playersInformation.length}</h2>
       <div className="flex justify-between items-center py-5">
-        <h2>Available Players</h2>
+        <h2 className="text-xl font-bold">Available Players</h2>
         <div className="flex">
-          <button className="btn btn-accent">Available</button>
-          <button className="btn ">Selected</button>
+          <button
+            onClick={() => handleButtonType("available")}
+            className={`btn ${buttonType === "available" ? "btn-success" : ""} rounded-r-none`}
+          >
+            Available
+          </button>
+          <button
+            onClick={() => handleButtonType("selected")}
+            className={`btn ${buttonType === "selected" ? "btn-success" : ""}      rounded-l-none`}
+          >
+            Selected
+          </button>
         </div>
       </div>
-        <div >
-            {
-                <AvailablePlayer playersInformation={playersInformation}></AvailablePlayer>
-            }
-            </div>
+      <div>{<AvailablePlayer playersInformation={playersInformation}></AvailablePlayer>}</div>
     </div>
   );
 };
